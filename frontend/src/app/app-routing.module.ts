913b639fc248210b0ef92ac8b6dbead2f00641de
marcioRLopez <ciomar2010@gmail.com>
2023-01-02 17:22:44 -0300

    agregando otros componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent},
+  { path: 'footer', component: FooterComponent},
+  { path: 'login', component: LoginComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
