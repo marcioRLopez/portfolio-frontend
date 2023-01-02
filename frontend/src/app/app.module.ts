7156afa4d5875c7b348b6ca7c281e67865cc219e
marcioRLopez <ciomar2010@gmail.com>
2023-01-02 20:46:19 -0300

    agregado de componentes: banner, hard soft skills , educacion, acerca de

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    HardSoftSkillsComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
